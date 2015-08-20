c64e5718a513032c02987167de01f9a044ff7ac1
Warren Wong <wrrnwng@gmail.com>
2015-08-19 21:58:07 -0700

    completed 3.5.1: extend, retract chat slider

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -17,21 +17,89 @@ spa.shell = (function() {
       '</div>' +
       '<div class="spa-shell-foot"></div>' +
       '<div class="spa-shell-chat"></div>' +
-      '<div class="spa-shell-modal"></div>'
+      '<div class="spa-shell-modal"></div>',
+
+    chatExtendTime: 1000,
+    chatRetractTime: 300,
+    chatExtendHeight: 450,
+    chatRetractHeight: 15
   };
   var stateMap = { $container: null };
   var jqueryMap = {};
-  var setJqueryMap, initModule;
+  var setJqueryMap, toggleChat, initModule;
 
   setJqueryMap = function() {
     var $container = stateMap.$container;
-    jqueryMap = { $container: $container };
+    jqueryMap = {
+      $container: $container,
+      $chat: $container.find('.spa-shell-chat')
+    };
   };
+
+  /** DOM method /toggleChat
+   * Purpose: Extends or retracts chat slider
+   * Arguments:
+   *  doExtend - if true, extends slider, if false retracts
+   *  callback - optional function to execute at end of animation
+   * Settings:
+   *  chatExtendTime, chatRetractTime
+   *  chatExtendHeight, chatRetractHeight
+   * Returns: boolean
+   *  true - slider animation activated
+   *  false - slider animation not activated
+   */
+  toggleChat = function(doExtend, callback) {
+    var pxChatHt = jqueryMap.$chat.height();
+    var isOpen = pxChatHt === configMap.chatExtendHeight;
+    var isClosed = pxChatHt === configMap.chatRetractHeight;
+    var isSliding = !isOpen && !isClosed;
+
+    // avoid race condition
+    if (isSliding) {
+      return false;
+    }
+
+    // Begin extend chat slider
+    if (doExtend) {
+      jqueryMap.$chat.animate(
+        {height: configMap.chatExtendHeight},
+        configMap.chatExtendTime,
+        function() {
+          if (callback) {
+            callback(jqueryMap.$chat);
+          }
+        }
+      );
+      return true;
+    }
+
+    // Retract chat slider
+    jqueryMap.$chat.animate(
+      {height: configMap.chatRetractHeight},
+      configMap.chatRetractTime,
+      function() {
+        if (callback) {
+          callback(jqueryMap.$chat);
+        }
+      }
+    );
+
+    return true;
+  };
+
 
   initModule = function($container) {
     stateMap.$container = $container;
     $container.html(configMap.main_html);
     setJqueryMap();
+
+    // test toggle
+    setTimeout(function() {
+      toggleChat(true);
+    }, 3000);
+    setTimeout(function() {
+      toggleChat(false);
+    }, 8000);
   };
 
   return { initModule: initModule };
